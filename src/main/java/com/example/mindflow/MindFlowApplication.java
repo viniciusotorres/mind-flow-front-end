114db3c7494ce1dfc3f55805dec80794dc901625
@viniciusotorres <viniciustorres29@gmail.com>
2024-01-17 22:11:19 -0300

    Primeiro commit

---
+++ src/main/java/com/example/mindflow/MindFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mindflow/MindFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
